9eb6fefa833907e19665ada29604289448db8ca7
Rohan-Shridhar <rohansm668@gmail.com>
2025-12-23 14:04:53 +0530

    commit 1
    removed test case helper from code

--- Tournament_of_game_theory.java
+++ Tournament_of_game_theory.java
@@ -13,9 +13,7 @@ public class Tournament_of_game_theory {
     }
     public static void main(String[] args) {
         for(int i=0;i<players_count;i++){
-            //System.out.println(i);
             for(int j=i;j<players_count;j++){
-                //System.out.println(j);
                 Game_theory.score[0]=0;
                 Game_theory.score[1]=0;
                 play(i+1,j+1);
@@ -45,35 +43,28 @@ public class Tournament_of_game_theory {
             }
             return;
         }
-
         quickSort(player_children, 0, player_children.length - 1);
     }
 
     private static void quickSort(int[] arr, int low, int high) {
         if (low < high) {
             int pi = partition(arr, low, high);
-
             quickSort(arr, low, pi - 1);
-
             quickSort(arr, pi + 1, high);
         }
     }
 
     private static int partition(int[] arr, int low, int high) {
         int pivot = arr[high];
-
         int i = (low - 1);
-
         for (int j = low; j < high; j++) {
             if (arr[j] <= pivot) {
                 i++;
-
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
         }
-
         int temp = arr[i + 1];
         arr[i + 1] = arr[high];
         arr[high] = temp;
